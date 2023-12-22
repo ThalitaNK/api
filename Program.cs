2bbf2baae6385251d6ea475a3e165f869ec26216
Thalita Meira <thalita_meira@hotmail.com>
2023-12-22 13:07:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World! Apocalipse is near");
+
+app.Run();
+// app.MapPost("/", () => new {Name = "Thalita MEira", Age = 30 });
+// app.MapGet("/AddHeader",(HttpResponse response) => {
+//     response.Headers.Add("Teste", "Thalita Meira");
+//     return new {Name = "Thalita Meira", Age = 30};
+// });
+
+// app.MapPost("/saveproduct", (Product product) => {
+//     return product.Code + " - " + product.Name;
+
+// });
+
+// app.MapGet("/getproduct", ([FromQuery] string dateStart, [FromQuery] string dateEnd) => {
+//     return dateStart + " - " + dateEnd;
+// });
+
+// app.MapGet("/getproduct/{code}", (string code) => {
+//     return code;
+// });
+ 
+// public class Product {
+//     public string Code { get; set;}
+//     public string Name { get; set;}
+// }
