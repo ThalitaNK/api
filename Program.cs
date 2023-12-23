9a375178e906466d2efb6df61e3aa2eaf6884db7
Thalita Meira <thalita_meira@hotmail.com>
2023-12-22 21:28:16 -0300

    parametros pela URL

--- Program.cs
+++ Program.cs
@@ -10,22 +10,22 @@ app.MapGet("/AddHeader",(HttpResponse response) => {
      return new {Name = "Thalita Meira", Age = 30};
  });
 
-// app.MapPost("/saveproduct", (Product product) => {
-//     return product.Code + " - " + product.Name;
+ app.MapPost("/saveproduct", (Product product) => {
+     return product.Code + " - " + product.Name;
 
-// });
+ });
 
-// app.MapGet("/getproduct", ([FromQuery] string dateStart, [FromQuery] string dateEnd) => {
-//     return dateStart + " - " + dateEnd;
-// });
+ app.MapGet("/getproduct", ([FromQuery] string dateStart, [FromQuery] string dateEnd) => {
+     return dateStart + " - " + dateEnd;
+ });
 
-// app.MapGet("/getproduct/{code}", (string code) => {
-//     return code;
-// });
+ app.MapGet("/getproduct/{code}", ([FromRoute] string code) => {
+     return code;
+ });
 
 app.Run();
  
-// public class Product {
-//     public string Code { get; set;}
-//     public string Name { get; set;}
-// }
+ public class Product {
+     public string Code { get; set;}
+     public string Name { get; set;}
+ }
