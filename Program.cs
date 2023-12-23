96d9667f864674de0090b9cc08e595e1b62f4de0
Thalita Meira <thalita_meira@hotmail.com>
2023-12-22 22:35:46 -0300

    adiciona repositório de dados usando static

--- Program.cs
+++ Program.cs
@@ -28,6 +28,29 @@ app.MapGet("/AddHeader",(HttpResponse response) => {
  });
 
 app.Run();
+
+/// <summary>
+/// o static faz com que essa classe continue funcionando na memória mesmo que a gente faça requisição após requisição. 
+/// Sempre que usamos static em uma classe, significa que vai ficar na memória do servidor e vai ser disponibilizado
+/// entre qualquer requisição. Ela se torna global. Se uma classe é static, todos os métodos dentro dela tbm precisam ser.
+/// </summary>
+public static class ProductRepository 
+{
+    public static List<Product> Products { get; set; }
+
+    public static void Add(Product product) 
+    {
+        if(Products == null)
+            Products = new List<Product>();
+
+            Products.Add(product);
+    }
+
+    public static Product GetBy(string code) {
+         return Products.First(p => p.Code == code);
+    }
+
+}
  
  public class Product {
      public string Code { get; set;}
