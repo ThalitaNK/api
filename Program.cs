6b126d88749c592e59d46a572e84b1529db0aa61
Thalita Meira <thalita_meira@hotmail.com>
2023-12-24 16:28:49 -0300

    adiciona rota para deletar dados

--- Program.cs
+++ Program.cs
@@ -36,13 +36,31 @@ internal class Program
         {
             return request.Headers["product-code"].ToString();
         });
-        
+
         ///para editar o produto
         app.MapPut("/editproduct", (Product product) =>
         {
             var productSaved = ProductRepository.GetBy(product.Code);
             productSaved.Name = product.Name;
             return productSaved;
+        });
+
+        app.MapDelete("/deleteproduct/{code}", ([FromRoute] string code) =>
+        {
+            var productSaved = ProductRepository.GetBy(code);
+            ProductRepository.Remove(productSaved);
+            
+            if (productSaved != null)
+            {
+                ProductRepository.Remove(productSaved);
+                string message = $"Product with code {code} deleted";
+                return Results.Ok(message);
+            }
+            else
+            {
+                string errorMessage = $"Product with code {code} not found";
+                return Results.NotFound(errorMessage);
+            }
         });
 
         app.Run();
@@ -54,25 +72,34 @@ internal class Program
 /// Sempre que usamos static em uma classe, significa que vai ficar na memória do servidor e vai ser disponibilizado
 /// entre qualquer requisição. Ela se torna global. Se uma classe é static, todos os métodos dentro dela tbm precisam ser.
 /// </summary>
-public static class ProductRepository 
+public static class ProductRepository
 {
     public static List<Product> Products { get; set; }
 
-    public static void Add(Product product) 
+    public static void Add(Product product)
     {
-        if(Products == null)
+        if (Products == null)
             Products = new List<Product>();
 
-            Products.Add(product);
+        Products.Add(product);
     }
 
-    public static Product GetBy(string code) {
-         return Products.FirstOrDefault(p => p.Code == code);
+    public static Product GetBy(string code)
+    {
+        return Products.FirstOrDefault(p => p.Code == code);
+    }
+
+    public static void Remove(Product product)
+    {
+        Products.Remove(product);
     }
 
 }
- 
- public class Product {
-     public string Code { get; set;}
-     public string Name { get; set;}
- }
+
+
+
+public class Product
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
