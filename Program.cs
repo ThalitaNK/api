8b1a2da117ccc22ecf6fefe63cbe867507c354f8
Thalita Meira <thalita_meira@hotmail.com>
2023-12-25 16:02:52 -0300

    refatora endpoints

--- Program.cs
+++ Program.cs
@@ -7,27 +7,27 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
-        app.MapPost("/saveproduct", (Product product) =>
+        app.MapPost("/products", (Product product) =>
         {
             ProductRepository.Add(product);
 
         });
 
-        app.MapGet("/getproduct/{code}", ([FromRoute] string code) =>
+        app.MapGet("/products/{code}", ([FromRoute] string code) =>
         {
             var product = ProductRepository.GetBy(code);
             return product;
         });
 
         ///para editar o produto
-        app.MapPut("/editproduct", (Product product) =>
+        app.MapPut("/products", (Product product) =>
         {
             var productSaved = ProductRepository.GetBy(product.Code);
             productSaved.Name = product.Name;
             return productSaved;
         });
 
-        app.MapDelete("/deleteproduct/{code}", ([FromRoute] string code) =>
+        app.MapDelete("/products/{code}", ([FromRoute] string code) =>
         {
             var productSaved = ProductRepository.GetBy(code);
             ProductRepository.Remove(productSaved);
