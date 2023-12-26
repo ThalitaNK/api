c04b4d1f20a0581f92853a5e1753029aa9600c9e
Thalita Meira <thalita_meira@hotmail.com>
2023-12-26 20:39:57 -0300

    atualiza branch

--- obj/Debug/net6.0/api.AssemblyInfo.cs
+++ obj/Debug/net6.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f545b5648ed22f5d2a17100a38284cccda281ebb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+446adcd8c9146e6e98bd48fc31e54898cbef201a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
