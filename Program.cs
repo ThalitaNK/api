334fd7199b21c072be4e5b87b9eea6ded786384d
Thalita Meira <thalita_meira@hotmail.com>
2024-01-04 17:10:38 -0300

    atualiza arquivo de configuração

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
+        var configuration = app.Configuration;
+        ProductRepository.Init(configuration);
 
         app.MapPost("/products", (Product product) =>
         {
@@ -49,7 +51,7 @@ internal class Program
         });
 
         app.MapGet("/configuration/database", (IConfiguration configuration) => {
-            return Results.Ok(configuration["database:connection"]);
+            return Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}");
 
         });
 
@@ -64,13 +66,14 @@ internal class Program
 /// </summary>
 public static class ProductRepository
 {
-    public static List<Product> Products { get; set; }
+    public static List<Product> Products { get; set; } = Products = new List<Product>();
 
-    public static void Add(Product product)
-    {
-        if (Products == null)
-            Products = new List<Product>();
+    public static void Init(IConfiguration configuration){
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
 
+    public static void Add(Product product) {
         Products.Add(product);
     }
 
